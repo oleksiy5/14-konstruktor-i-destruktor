1da7e88f59558d05f31a05de7e883e3cc28bb32b
Oleksiy Nekrasov <n.507592979@gmail.com>
2018-04-23 17:14:48 +0200

    d

---
+++ ProjContructorAndDestructor/ProjContructorAndDestructor/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ProjContructorAndDestructor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Ball ball = new Ball("Ball_1"); //<--object ball
+
+            Console.WriteLine("...");
+            Thread.Sleep(2000);
+
+
+            ball = null; //<--usuwam referencje
+
+            GC.Collect(); /* <-- wymuszam uruchomienie Garbage Collector'a 
+                           *     który niszczy wszystkie obiekty 
+                           *     bez referencji
+                           */
+
+            Console.ReadKey();
+
+        }   
+    }
+
+    class Ball
+    {
+        public Ball(string name)
+        {
+            Console.WriteLine($"[1] The object {name} is created.");
+        }
+
+
+        ~Ball()
+        {
+            Console.WriteLine($"[2] The object is destroyed.");
+        }
+    }
+
+}
